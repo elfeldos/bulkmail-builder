b64f9e2a5088846204f8a9813fc362e8349d2e6e
Frederik Feldmeier <74424903+elfeldos@users.noreply.github.com>
2025-01-04 19:59:12 +0100

    added posthog

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,19 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { PostHogProvider} from 'posthog-js/react'
 
-createRoot(document.getElementById("root")!).render(<App />);
+const options = {
+    api_host: import.meta.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+  }
+  
+createRoot(document.getElementById('root')!).render(
+<PostHogProvider 
+    apiKey={import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_KEY}
+    options={options}
+>
+    <App />
+</PostHogProvider>
+);
+
+// createRoot(document.getElementById("root")!).render(<App />);
